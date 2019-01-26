6d5916b5de50483362d40758ef8f3613f802a580
John Flinchbaugh <john@hjsoft.com>
2019-01-26 16:44:13 -0500

    modular

---
+++ ffs.js
@@ -0,0 +1,21 @@
+let fs = require('fs')
+let path = require('path')
+
+module.exports = function(dir, ext, callback) {
+    fs.readdir(
+        dir,
+        'utf8',
+        (err, data) => {
+            if (err) {
+                callback(err, null)
+                return
+            }
+            callback(
+                null,
+                data.filter(
+                    f => path.extname(f) === ('.' + ext)
+                )
+            )
+        }
+    )
+}
---
+++ module_filtered.js
@@ -0,0 +1,9 @@
+let ffs = require('./ffs')
+
+ffs(
+    process.argv[2],
+    process.argv[3],
+    (err, data) => {
+        data.forEach(f => console.log(f))
+    }
+)
