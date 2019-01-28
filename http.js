7c81743651c7bd619e975cf47644b94186222e7a
John Flinchbaugh <john@hjsoft.com>
2019-01-28 13:25:06 -0500

    http

---
+++ http.js
@@ -0,0 +1,14 @@
+let http = require('http')
+
+http.get(
+    process.argv[2],
+    response => {
+        response.setEncoding('utf8')
+        response.on(
+            'data',
+            data => {
+                console.log(data)
+            }
+        )
+    }
+)
