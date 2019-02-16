c3d27e43d46add39933fae57c0d98dc9f2480f69
John Flinchbaugh <john@hjsoft.com>
2019-02-16 12:44:38 -0500

    use the const iterator variable

--- async_http.js
+++ async_http.js
@@ -17,16 +17,15 @@ const done = (lst =>
     )
 )
 
-for (idx in [0, 1, 2]) {
-    const i = idx
+for (const idx in [0, 1, 2]) {
     http.get(
-        urls[i],
+        urls[idx],
         response => {
             response.setEncoding('utf8')
             response.pipe(
                 concatStream(
                     data => {
-                        results[i] = data
+                        results[idx] = data
                         if (done(results)) {
                             print(results)
                         }
